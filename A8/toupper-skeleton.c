c4502694c480825123302b5eeecdda2cf2c0c5f0
Helmut Joaquín Pfeffer García <69730072+Jimnav-png@users.noreply.github.com>
2022-12-11 17:12:15 +0100

    Finished toupper

--- A8/toupper-skeleton.c
+++ A8/toupper-skeleton.c
@@ -4,8 +4,8 @@ int main() {
     int c;
 
     while((c = getchar()) != EOF) { // read until end of input
-        if( /* your code */ ) // if letter, change case
-            /* your code to do the case change */
+        if( c >= 97 && c <= 122) // if letter, change case
+            c &= ~(1UL << 5);
         putchar(c);
     }
     return 0;
